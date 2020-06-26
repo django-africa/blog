fd991e05c5e8da6098239e94274806b680ab53e5
marvelous-ikechi <marvelike7@gmail.com>
2020-06-26 11:50:02 +0100

    comments modified

--- blogApi/urls.py
+++ blogApi/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.conf import settings
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 from django.conf.urls.static import static
 from .views import UserViewSet, PostViewSet, CommentViewSet
 
-router = SimpleRouter()
+router = DefaultRouter()
 router.register('users', UserViewSet, basename='users')
-router.register('', PostViewSet, basename='posts')
+router.register('posts', PostViewSet, basename='posts')
 router.register('comments', CommentViewSet, basename='comments')
 
 urlpatterns = router.urls + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
