3f05271b2a93bf676d4ecfe28ccdcf3b66edd678
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2020-06-28 00:45:32 +0100

    added static root

--- blogApiProject/settings.py
+++ blogApiProject/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =
+STATIC_ROOT = '/var/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
