18572a720d5aba0870449efa2dd55e486c33c8da
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2020-06-28 00:21:18 +0100

    requirememts.txt

--- blogApiProject/settings.py
+++ blogApiProject/settings.py
@@ -159,5 +159,6 @@ BROKER_URL = [os.environ.get('REDIS_URL', 'redis://localhost:6379')]
 CELERY_RESULT_BACKEND = [os.environ.get('REDIS_URL', 'redis://localhost:6379')]
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
+CELERY_TASK_ALWAYS_EAGER = False
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Africa/Lagos'
