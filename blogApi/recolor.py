90897b258265faccdb21897c9c6a3065a35125da
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2020-06-28 03:39:22 +0100

    .

--- blogApi/recolor.py
+++ blogApi/recolor.py
@@ -1,50 +1,50 @@
 from PIL import Image
 
-def open_image(path):
-        newImage = Image.open(path)
-    return newImage
+# def open_image(path):
+#         newImage = Image.open(path)
+#     return newImage
 
-def save_image(image, path):
-    image.save(path, 'png')
+# def save_image(image, path):
+#     image.save(path, 'png')
 
-def create_image(i, j):
-        image = Image.new("RGB", (i, j), "white")
-    return image
+# def create_image(i, j):
+#         image = Image.new("RGB", (i, j), "white")
+#     return image
 
-# Get the pixel from the given image
-def get_pixel(image, i, j):
-    # Inside image bounds?
-    width, height = image.size
-    if i > width or j > height:
-        return None
+# # Get the pixel from the given image
+# def get_pixel(image, i, j):
+#     # Inside image bounds?
+#     width, height = image.size
+#     if i > width or j > height:
+#         return None
 
-    # Get Pixel
-    pixel = image.getpixel((i, j))
-    return pixel
+#     # Get Pixel
+#     pixel = image.getpixel((i, j))
+#     return pixel
 
-    # Create new Image and a Pixel Map
-    new = create_image(width, height)
-    pixels = new.load()
+#     # Create new Image and a Pixel Map
+#     new = create_image(width, height)
+#     pixels = new.load()
 
-  # Transform to bluescale
-  for i in range(width):
-    for j in range(height):
-      # Get Pixel
-      pixel = get_pixel(image, i, j)
+#     # Transform to bluescale
+#     for i in range(width):
+#     for j in range(height):
+#         # Get Pixel
+#         pixel = get_pixel(image, i, j)
 
-      # Get R, G, B values (This are int from 0 to 255)
-      red =   pixel[0]
-      green = pixel[1]
-      blue =  pixel[2]
+#         # Get R, G, B values (This are int from 0 to 255)
+#         red =   pixel[0]
+#         green = pixel[1]
+#         blue =  pixel[2]
 
-      # Transform to grayscale
-      gray = (red * 0.299) + (green * 0.587) + (blue * 0.700)
+#         # Transform to grayscale
+#         gray = (red * 0.299) + (green * 0.587) + (blue * 0.700)
 
-      # Set Pixel in new image
-      pixels[i, j] = (int(gray), int(gray), int(gray))
+#         # Set Pixel in new image
+#         pixels[i, j] = (int(gray), int(gray), int(gray))
 
-    # Return new image
-    return new
+#     # Return new image
+#     return new
 
 def recolor_blog_picture(picture):
     pass
