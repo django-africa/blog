7fdf5dd7323e5964319ea307f77388ccbfd26206
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2020-07-04 11:52:45 +0100

    fixing the user registration

--- blogApi/views.py
+++ blogApi/views.py
@@ -19,7 +19,18 @@ class PostViewSet(viewsets.ModelViewSet, HitCountDetailView):
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserProfileSerializer
     queryset = UserProfile.objects.all()
+    """   
+    Create User
+    """
+    @action(detail=False, methods=['post'], permission_classes=[AllowAny])
+    def post(self, request, format='json'):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
