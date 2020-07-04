915e0948b2ee1b07f80dbd87c721885a0fea328e
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2020-07-04 10:52:54 +0100

    fixing the user registration

--- blogApi/serializers.py
+++ blogApi/serializers.py
@@ -1,13 +1,31 @@
 from rest_framework import serializers, fields
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth import get_user_model
 from taggit_serializer.serializers import TagListSerializerField, TaggitSerializer
 from .models import Post, Comment, UserProfile, PrayerRequest, Category
   
 
+User = get_user_model()
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+            required=True,
+            validators=[UniqueValidator(queryset=User.objects.all())]
+            )
+    username = serializers.CharField(
+            max_length=32,
+            validators=[UniqueValidator(queryset=User.objects.all())]
+            )
+    password = serializers.CharField(min_length=8, write_only=True)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'],
+             validated_data['password'])
+        return user
+
     class Meta:
-        fields = '__all__'
         model = UserProfile
+        fields = ('id', 'username', 'email', 'password')
 
 
 class CategorySerializer(serializers.ModelSerializer):
--- blogApi/views.py
+++ blogApi/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
-from django.views.generic import FormView
-from django.contrib.auth.forms import UserCreationForm
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework import generics, viewsets
 from .serializers import PostSerializer, CommentSerializer, UserProfileSerializer, PrayerRequestSerializer, CategorySerializer
@@ -35,18 +35,18 @@ class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
 
-class Registration(FormView):
-    form_class = UserCreationForm
-    template_name = "blogApi/registration.html"
-    success_url = "/"
+class Registration(APIView):
+    """
+    Create User
 
-    
-    # def post( self, request):
-    #     user = User.object.create(
-    #         username=request.data.get('username'),
-    #         email=request.data.get('email'),
-    #     )
-    #     user.set_password(str(request.data.get('password')))
-    #     user.save()
+    """
 
-    #     return Response({"status": "sucess", "response":"User Sucessfully Created"}, status.status.HTTP_201_CREATED)
+    def post(self, request, format='json'):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                token = Token.objects.create(user=user)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
