2b0a77f6975df61891b9a5485235a5e5adacba22
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2020-07-18 20:43:46 +0100

    mod

--- blogApi/serializers.py
+++ blogApi/serializers.py
@@ -3,18 +3,11 @@ from rest_framework.validators import UniqueValidator
 from django.contrib.auth import get_user_model
 from taggit_serializer.serializers import TagListSerializerField, TaggitSerializer
 from .models import Post, Comment, UserProfile, PrayerRequest, Advert, Category, Contact, Publication
-from drf_extra_fields.fields import Base64FileField
-import base64
-from django.core.files.base import ContentFile
+from drf_extra_fields.fields import Base64ImageField
+
 
 User = get_user_model()
 
-def base64_file(data, name=None):
-    _format, _img_str = data.split(';base64,')
-    _name, ext = _format.split('/')
-    if not name:
-        name = _name.split(":")[-1]
-    return ContentFile(base64.b64decode(_img_str))
 
 class UserProfileSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
@@ -45,7 +38,7 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class PostSerializer(serializers.ModelSerializer, TaggitSerializer):
     created_date = fields.DateTimeField(input_formats=['%Y-%m-%d:%H:%M:%S'], required=False)
-    featured_image = Base64FileField()
+    featured_image = Base64ImageField()
     published_date = fields.DateTimeField(input_formats=['%Y-%m-%d:%H:%M:%S'], required=False)
     category = CategorySerializer(required=False)
     view_count = fields.IntegerField(source='get_count', required=False)
@@ -78,7 +71,8 @@ class ContactSearlizer(serializers.ModelSerializer):
         model = Contact
 
 class AdvertSearlizer(serializers.ModelSerializer):
-    photo = Base64FileField()
+    photo = Base64ImageField()
+
     class Meta:
         fields = '__all__'
         model = Advert
@@ -89,7 +83,8 @@ class AdvertSearlizer(serializers.ModelSerializer):
     #     return super(AdvertSerializer, self).save(**kwargs)
 
 class PublicationSerializer(serializers.ModelSerializer):
-    photo = Base64FileField()
+    photo = Base64ImageField()
+    
     class Meta:
         fields = '__all__'
         model = Publication
