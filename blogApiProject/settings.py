d0f47e5bddc93ff7978f0523651607a2826db036
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2020-07-23 17:55:48 +0100

    added default file storage

--- blogApiProject/settings.py
+++ blogApiProject/settings.py
@@ -142,7 +142,7 @@ AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 # s3 static settings
 AWS_LOCATION = 'static'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+DEFAULT_FILE_STORAGE = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # STATIC_URL = '/static/'
