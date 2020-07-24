3b392c442d087605733718cf9855b36b0b47016e
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2020-07-24 17:14:08 +0100

    mod admin

--- blogApi/admin.py
+++ blogApi/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from .models import Post, Comment, UserProfile, Category, PrayerRequest
+from .models import Post, Comment, UserProfile, Category, PrayerRequest, Event, Ministries, Advert, Contact, Publication, 
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(UserProfile)
 admin.site.register(Category)
 admin.site.register(PrayerRequest)
+admin.site.register(Event)
+admin.site.register(Ministries)
+admin.site.register(Advert)
+admin.site.register(Contact)
+admin.site.register(Publication)
+admin.site.register(Comment)
 
 
 @admin.register(Comment)
