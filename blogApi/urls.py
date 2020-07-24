9c265a1d4eed5a99ba7db04d6499e664d871d7fc
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2020-07-24 13:06:09 +0100

    added suport for event

--- blogApi/urls.py
+++ blogApi/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 # from django.contrib.staticfiles import views
 # from django.urls import re_path
-from .views import UserViewSet,EventSerializer, PostViewSet, AdvertViewSet, ContactViewSet, CommentViewSet, PrayerRequestViewSet, CategoryViewSet, PublicationViewSet, MinistriesViewset
+from .views import UserViewSet, EventViewset, PostViewSet, AdvertViewSet, ContactViewSet, CommentViewSet, PrayerRequestViewSet, CategoryViewSet, PublicationViewSet, MinistriesViewset
 
 router = DefaultRouter()
 router.register('users', UserViewSet, basename='users')
