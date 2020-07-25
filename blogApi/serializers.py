a4b7daf45c0f22456fc58f5734e82a9d5bf312d5
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2020-07-25 13:39:41 +0100

    mod user fields searilizer

--- blogApi/serializers.py
+++ blogApi/serializers.py
@@ -27,7 +27,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserProfile
-        fields = ('id', 'username', 'email', 'password')
+        fields = '__all__'
 
 
 class CategorySerializer(serializers.ModelSerializer):
