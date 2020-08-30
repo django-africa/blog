886c061f49e4b728f5ae109890860adfdedba39f
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2020-08-30 02:11:57 +0100

    debug false

--- blogApiProject/urls.py
+++ blogApiProject/urls.py
@@ -24,7 +24,7 @@ API_DESCRIPTION = 'A web API for creating and editing post'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-token-auth/', obtain_jwt_token),
-    # path('',  include('blogApi.urls')),
+    path('',  include('blogApi.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
