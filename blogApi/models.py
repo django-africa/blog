f48c58810262cbe34b88dc87ccbc6308779f9e15
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2020-08-30 11:19:31 +0100

    debug fix

--- blogApi/models.py
+++ blogApi/models.py
@@ -31,7 +31,7 @@ class Post(models.Model, HitCountMixin):
     author = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=350, unique=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
-    text = models.TextField(default="")
+    text = models.TextField()
     view_count = models.IntegerField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     featured_image = models.ImageField(upload_to='images/', null=True, blank=True)
